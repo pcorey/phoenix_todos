a7803b8651f24973614a608c94817f9235099ecb
Pete Corey <petecorey@gmail.com>
2016-09-20 17:38:45 -0400

    # Seeding Data
    Now that we've defined our schemas and models, we need to populate our
    database with seed data.
    
    Before we do anything, we need to make sure that our migrations are up
    to date:
    
        mix ecto.migrate
    
    This will create our `"lists"` and `"todos"` tables.
    
    Now we can start writing our seeding script. We'll model this script
    after the [original `fixtures.js`
    file](https://github.com/meteor/todos/blob/master/imports/startup/server/fixtures.js)
    in our Meteor application.
    
    We'll start by creating a list of in-memory lists and todos that we'll
    use to build our database objects:
    
        [
          %{
            name: "Meteor Principles",
            items: [
              "Data on the Wire",
              "One Language",
              "Database Everywhere",
              "Latency Compensation",
              "Full Stack Reactivity",
              "Embrace the Ecosystem",
              "Simplicity Equals Productivity",
            ]
          },
          ...
        ]
    
    Notice that we're using double quote strings here instead of single
    quote strings, like the original Meteor appliaction. This is because
    single quote strings have a [special
    meaning](http://elixir-lang.org/getting-started/binaries-strings-and-char-lists.html#char-lists)
    in Elixir.
    
    Next, we'll `map` over each of object in this list. Each object
    represents a `List`, so we'll build a `List` model object and `insert`
    it into our database:
    
        list = Repo.insert!(%List{
          name: data.name,
          incomplete_count: length(data.items)
        })
    
    Each string in `list.items` represents a single `Todo`. We'll map over
    this list build a new `Todo` model object, associating it with the
    `List` we just created using `Ecto.build_assoc`, and inserting it into
    the database:
    
        Ecto.build_assoc(list, :todos, text: item)
        |> Repo.insert!
    
    Now we can run our seed script with the following command:
    
        mix run priv/repo/seeds.exs
    
    Or, we can wipe our database and re-run our migrations and seed script
    with the following command:
    
        mix ecto.reset
    
    After running either of these, our database should have three lists,
    each with a set of associated todos.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,55 @@
 # Script for populating the database. You can run it as:
 #
 #     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     PhoenixTodos.Repo.insert!(%PhoenixTodos.SomeModel{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+
+alias PhoenixTodos.{Repo, List}
+
+[
+  %{
+    name: "Meteor Principles",
+    items: [
+      "Data on the Wire",
+      "One Language",
+      "Database Everywhere",
+      "Latency Compensation",
+      "Full Stack Reactivity",
+      "Embrace the Ecosystem",
+      "Simplicity Equals Productivity",
+    ]
+  },
+  %{
+    name: "Languages",
+    items: [
+      "Lisp",
+      "C",
+      "C++",
+      "Python",
+      "Ruby",
+      "JavaScript",
+      "Scala",
+      "Erlang",
+      "6502 Assembly",
+    ]
+  },
+  %{
+    name: "Favorite Scientists",
+    items: [
+      "Ada Lovelace",
+      "Grace Hopper",
+      "Marie Curie",
+      "Carl Friedrich Gauss",
+      "Nikola Tesla",
+      "Claude Shannon",
+    ]
+  }
+]
+|> Enum.map(fn data ->
+  list = Repo.insert!(%List{
+    name: data.name,
+    incomplete_count: length(data.items)
+  })
+  Enum.map(data.items, fn item ->
+    Ecto.build_assoc(list, :todos, text: item)
+    |> Repo.insert!
+  end)
+end)
